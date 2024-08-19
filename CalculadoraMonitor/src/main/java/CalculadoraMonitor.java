053d2bc48a27ebd99aebd064926d3131892c7946
Luna-Scott <est.luna.scott@unimilitar.edu.co>
2024-08-18 23:14:44 -0500

    #1 Calculadora Monitor
    Este código establece la estructura básica para la calculadora que recibe entrada del usuario

---
+++ CalculadoraMonitor/src/main/java/CalculadoraMonitor.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CalculadoraMonitor {
+    
+    public static void main(String[] args) {
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        boolean continuar = true; 
+    }
+}

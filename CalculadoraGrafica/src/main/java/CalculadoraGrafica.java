1fc462c424691e9ea505cd39f2469a96f8532650
Luna-Scott <est.luna.scott@unimilitar.edu.co>
2024-08-19 20:40:16 -0500

    #4 Calculadora Grafica
    Se agrego la parte final del código en la que se asegura que todo se ejecute correctamente con el Swing

--- CalculadoraGrafica/src/main/java/CalculadoraGrafica.java
+++ CalculadoraGrafica/src/main/java/CalculadoraGrafica.java
@@ -104,5 +104,17 @@ public CalculadoraGrafica() {
             display.setText("");
         }
     }
+    
+   public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        SwingUtilities.invokeLater(() -> {
+            CalculadoraGrafica calculadora = new CalculadoraGrafica();
+            calculadora.setVisible(true);
+        });
+    }
 }
